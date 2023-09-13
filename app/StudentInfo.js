701a479ed53b9dbf5256dcc1f2f6f5b497c4d924
donnysait88 <119543204+donnysait88@users.noreply.github.com>
2023-09-13 13:31:17 -0600

    Commit

---
+++ app/StudentInfo.js
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+const StudentInfo = () => {
+    return (
+    <div className="text-sm">
+        <h1>My Student Info</h1>
+        <p>Name: Paradon Meeanan</p>
+        <p>Course section: CPRG 306 E</p>
+        <nav>
+            <Link href="https://github.com/donnysait88/cprg306-assignments">https://github.com/donnysait88/cprg306-assignments</Link>
+        </nav>
+    </div>
+    )
+  };
+
+
+export default StudentInfo;
--- app/week2/page.js
+++ app/week2/page.js
@@ -1,7 +1,6 @@
-import StudentInfo from "./StudentInfo";
-import Link from "next/link";
+import StudentInfo from "../StudentInfo";
 
-export default function Home() {
+const myShop = () => {
   return (
 
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -9,16 +8,14 @@ export default function Home() {
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono lg:flex">
 
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center text-4xl bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <h1>CPRG 306: Web Development 2 - Assignments</h1>
+          <h1>My Shopping List</h1>
         </div>
 
         <StudentInfo />
-
-        <div className="text-sm">
-          <Link href="./week2">Week 2</Link>
-        </div>
-
       </div>
+      
     </main>
   )
 }
+
+export default myShop;
