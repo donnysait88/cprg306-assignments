bab658304f7024683069a4a94c196e332b21bca6
donnysait88 <119543204+donnysait88@users.noreply.github.com>
2023-10-27 13:14:50 -0600

    Update page.js
    Add some details in page.js

--- app/week8/page.js
+++ app/week8/page.js
@@ -6,9 +6,6 @@ import Link from "next/link";
 export default function Login() {
   const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
 
-  // user is an object with the following properties:
-  // displayName, email, emailVerified, photoURL, uid, phoneNumber, providerData
-
   async function handleSignIn() {
     try {
       await gitHubSignIn();
@@ -27,9 +24,12 @@ export default function Login() {
 
   return (
     <div className="min-h-screen">
+      
       <div className="text-2xl font-sans font-bold m-8 ">
+        <h1>Welcome!!</h1>
         <h1>The Shopping List Application</h1>
       </div>
+
       {user ? (
         <div className="m-8">
           <div className="font-sans text-[20px]">
@@ -39,7 +39,7 @@ export default function Login() {
           <img src={user.photoURL} alt="user avatar" className="w-30 h-40 mt-4 mb-4 rounded-lg" />
           <div className="absolute flex flex-col">
             <div className="text-lg p-4 bg-green-700 text-white rounded-lg hover:bg-green-500 mb-4 cursor-pointer">
-              <Link href="./week8/shopping-list">Let go shopping....</Link>
+              <Link href="./week8/shopping-list">Let go shopping .. &#127822; </Link>
             </div>
             <button
               onClick={handleSignOut}
